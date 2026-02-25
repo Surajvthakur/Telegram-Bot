8d882e64d0990b2ba6f8b25ce1756254440c13d4
Surajvthakur <surajvt212gmail.com>
2026-02-25 17:48:47 +0530

    added router in main graph. Decides whether the agent should use a tool or respond directly

--- backend/app/graphs/main_graph.py
+++ backend/app/graphs/main_graph.py
@@ -1,6 +1,16 @@
 from app.core.llm import llm
 from langgraph.prebuilt import create_react_agent
 from app.tools.telegram import telegram
+from app.schemas.agent_state import AgentState
+
+# Define the router function
+def router(state: AgentState):
+    last = state["messages"][-1].content
+
+    if "search" in last.lower():
+        return "tool"
+
+    return "respond"
 
 # Define tools available to the agent
 tools = [] # Add tools here as they are developed
